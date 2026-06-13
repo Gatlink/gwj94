05c9a5fe1b6309b9b5bf72b4fb4fa3068082ece1
Gatlink <tyganit@gmail.com>
2026-06-13 17:32:38 +0200

    Character dummy

--- Character/States/stand.gd
+++ Character/States/stand.gd
@@ -5,6 +5,13 @@ extends CharacterState
 const SPEED := 5.0
 
 
+func _process(_delta: float) -> void:
+	if character.velocity:
+		character.dummy.walk()
+	else:
+		character.dummy.idle()
+
+
 func _physics_process(_delta: float) -> void:
 	# Movement
 	if PlayerInput.move_dir:
--- Character/character.gd
+++ Character/character.gd
@@ -7,7 +7,7 @@ static var instance: Character
 
 @onready var graph: Node3D = $Graph
 @onready var input_prompt: Sprite3D = $InputPrompt
-@onready var viewport: Viewport = get_viewport()
+@onready var dummy: CharacterDummy = $Graph/CharacterDummy
 @onready var no_input: CharacterNoInput = $NoInput
 @onready var stand: CharacterStand = $Stand
 @onready var move_to: CharacterMoveTo = $MoveTo
