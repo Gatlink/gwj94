1c97a57d5e2188a0467a35d93d050d452e2ff100
Gatlink <tyganit@gmail.com>
2026-06-20 13:32:59 +0200

    Fix player texture + plug shotgun animations

--- Character/Player/player_dummy.gd
+++ Character/Player/player_dummy.gd
@@ -3,6 +3,8 @@ extends CharacterDummy
 
 
 const PARAMETER_WALK := "parameters/Stand/blend_position"
+const PARAMETER_WALK_FAST := "parameters/StandFast/blend_position"
+const PARAMETER_WALK_SHOTGUN := "parameters/StandShotgun/blend_position"
 const PARAMETER_SPEED := "parameters/Speed/scale"
 const PARAMETER_STATE := "parameters/State/transition_request"
 const PARAMETER_SHOOT := "parameters/Shoot/request"
@@ -13,11 +15,16 @@ const ANIM_SPEED_WALK_FACTOR := 0.8
 @onready var shotgun: MeshInstance3D = $ShotGun
 
 
+var parameter_walk := PARAMETER_WALK
+
+
 func _ready() -> void:
 	if not Game.is_locked(Game.UPGRADES.SHOTGUN):
+		parameter_walk = PARAMETER_WALK_SHOTGUN
 		animation.set(PARAMETER_STATE, "shotgun")
 		shotgun.show()
 	elif not Game.is_locked(Game.UPGRADES.SPEED):
+		parameter_walk = PARAMETER_WALK_FAST
 		animation.set(PARAMETER_STATE, "speed")
 	else:
 		animation.set(PARAMETER_STATE, "empty_hands")
@@ -32,7 +39,7 @@ func idle() -> void:
 
 
 func set_stand_parameter(direction: Vector2, speed := 1.0) -> void:
-	animation.set(PARAMETER_WALK, direction)
+	animation.set(parameter_walk, direction)
 	
 	if direction:
 		speed *= ANIM_SPEED_WALK_FACTOR
