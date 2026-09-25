042c3f183b174f0c1b0aca514912a3c6846bf1ad
Gatlink <tyganit@gmail.com>
2026-09-25 13:49:21 +0200

    Heal in shop

--- UI/Shop/shop.gd
+++ UI/Shop/shop.gd
@@ -10,6 +10,8 @@ const UPGRADE_BUTTON := preload("uid://ne1n42v0s2u2")
 @onready var label_money: Label = $MarginContainer/VBoxContainer/Top/Infos/LabelMoney
 @onready var label_floor_lock_4: Label = $MarginContainer/VBoxContainer/HBoxContainer/FloorLocks/LabelFloorLock4
 @onready var label_floor_lock_7: Label = $MarginContainer/VBoxContainer/HBoxContainer/FloorLocks/LabelFloorLock7
+@onready var life_bar: LifeBar = $MarginContainer/VBoxContainer/HBoxContainer2/HealingPanel/HBoxContainer/LifeBar
+@onready var heal_button: Button = $MarginContainer/VBoxContainer/HBoxContainer2/HealingPanel/HBoxContainer/HBoxContainer/HealButton
 
 
 var buttons: Array[UpgradeButton] = []
@@ -19,9 +21,12 @@ func _ready() -> void:
 	Upgrades.unlocked.connect(on_upgrade_bought)
 	
 	refresh_money()
+	
+	var floor_8 := Game.level.floors.find_custom(func (f: FloorData) -> bool: return f.available_upgrades >= 8)
+	var floor_12 := Game.level.floors.find_custom(func (f: FloorData) -> bool: return f.available_upgrades >= 12)
 	label_floor.text = "Floor %d" % Game.floor_nbr
-	#label_floor_lock_4.visible = Game.floor_nbr < LOCK_LEVELS[1]
-	#label_floor_lock_7.visible = Game.floor_nbr < LOCK_LEVELS[2]
+	label_floor_lock_4.visible = Game.floor_nbr <= floor_8
+	label_floor_lock_7.visible = Game.floor_nbr <= floor_12
 	
 	for upgrade in Upgrades.get_available_upgrades():
 		var button: UpgradeButton = UPGRADE_BUTTON.instantiate()
@@ -38,6 +43,8 @@ func _ready() -> void:
 	
 	if not buttons.is_empty():
 		buttons[0].grab_focus()
+	
+	refresh_health()
 
 
 func _exit_tree() -> void:
@@ -45,17 +52,36 @@ func _exit_tree() -> void:
 	Upgrades.unlocked.disconnect(on_upgrade_bought)
 
 
+func _on_heal_button_pressed() -> void:
+	Game.heal()
+	refresh()
+
+
 func on_upgrade_bought(upgrade: Upgrade) -> void:
-	refresh_money()
 	upgrade_info.refresh(upgrade)
-	
-	for button in buttons:
-		button.refresh(button.upgrade)
+	refresh()
 
 
 func on_upgrade_pressed(button: UpgradeButton) -> void:
 	button.grab_focus()
 
 
+func refresh() -> void:
+	refresh_money()
+	refresh_health()
+	refresh_ugprades()
+
+
 func refresh_money() -> void:
 	label_money.text = "$%d" % Game.money
+
+
+func refresh_ugprades() -> void:
+	for button in buttons:
+		button.refresh(button.upgrade)
+
+
+func refresh_health() -> void:
+	life_bar.refresh(Game.current_health, Game.max_health)
+	heal_button.disabled = Game.money < Game.heal_price or Game.current_health >= Game.max_health
+	heal_button.text = "$%d" % Game.heal_price
